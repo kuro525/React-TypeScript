829c215d00bf7a38abf0fd1501c825d434a1900a
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-08-24 00:06:16 +0900

    [FIX] Data リファクタリング

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,7 @@
 export type Row = {
   id: string
-  generation: string
-  answers: number[]
+  age: string
+  answers: (number | null)[]
 }
 
 export type Rows = Row[]
@@ -9,12 +9,12 @@ export type Rows = Row[]
 export const rows: Rows = [
   {
     id: 'up20-up30',
-    generation: '20〜30歳',
+    age: '20〜30歳',
     answers: [0.30, 0.20, 0.42, 0.18]
   },
   {
     id: 'up30-up40',
-    generation: '30〜40歳',
+    age: '30〜40歳',
     answers: [0.12, 0.30, 0.40, 0.18]
   }
 ]
