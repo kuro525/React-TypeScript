f300284dca82774f7f5bf221c4bc99b1e95b6703
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-08-24 00:00:28 +0900

    [ADD] thead Component

---
+++ src/thead.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const Comment: React.FC = () => (
+  <thead>
+  <tr>
+    <th>対象世帯</th>
+    <th>取り組んでいる</th>
+    <th>近いうちに取り組みたい</th>
+    <th>取り組んでいない</th>
+    <th>必要ない</th>
+  </tr>
+  </thead>
+)
+
+export default Comment
