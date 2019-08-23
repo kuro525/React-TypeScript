1933012115d20c7a9a396cc59f5a6b5ea28cb877
kuro525 <deep.impact525-2012-5-pc@hotmail.co.jp>
2019-08-24 00:01:04 +0900

    [ADD] App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {rows} from "./data"
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Thead from './thead'
+import Tbody from './tbody'
 
-export default App;
+
+const App: React.FC = () => (
+  <div>
+    <h1>調査</h1>
+    <Thead/>
+    <Tbody rows={rows}/>
+  </div>
+)
+
+export default App
